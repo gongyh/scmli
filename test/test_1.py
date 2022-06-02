ee600dc8990e3b3bc053ba3d147148d29a54e660
Yanhai Gong <gongyh@qibebt.ac.cn>
2022-06-02 14:04:29 +0800

    Update test_1.py

--- test/test_1.py
+++ test/test_1.py
@@ -1,7 +1,20 @@
 import pytest
+from ../scmli/pcr_pipline import pcr_qc, pcr_parse_gRNA, pcr_count
 
-# content of test_sample.py
-def func(x):
-    return x + 1
-def test_answer():
-    assert func(4) == 5
+def test_pcr():
+    '''
+    Test PCR pipeline
+    '''
+    # parameters
+    output_dir = 'my_project'
+    output_name = 'output'
+    read1 = 'test/test_R1.fq.gz'
+    read2 = 'test/test_R2.fq.gz'
+    lib = 'test/NoIMET1_gRNAs.csv'
+    seq = 'GGTAGAATTGGTCGTTGCCATCGACCAGGC'
+    number = '25 45'
+    # validate each step
+    pcr_qc(output_dir, output_name, read1, read2)
+    num_gRNAs = pcr_parse_gRNA(lib, seq, number, output_name)
+    pcr_count(output_name)
+    assert num_gRNAs == 10000
