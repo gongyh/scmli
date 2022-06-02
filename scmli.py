7179d1dec68be8bd37b738e72ffc69d0d40498a0
zhangjiayouc <zhangjs@qibebt.ac.cn>
2022-06-02 14:27:21 +0800

    change pipeline

--- scmli.py
+++ scmli.py
@@ -2,7 +2,7 @@
 
 import argparse
 import os
-from scmli.pcr_pipline import pcr_qc, pcr_parse_gRNA, pcr_count
+from scmli.pcr_pipeline import pcr_qc, pcr_parse_gRNA, pcr_count
 import pandas as pd
 
 
