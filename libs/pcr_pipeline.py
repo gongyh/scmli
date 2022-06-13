7fac0c5464c8c49094338fd9335392ef212e530d
zhangjiayouc <zhangjs@qibebt.ac.cn>
2022-06-13 12:23:19 +0800

    update multiprocess

--- libs/pcr_pipeline.py
+++ libs/pcr_pipeline.py
@@ -61,6 +61,7 @@ def pcr_parse_gRNA(lib, fix_seq, number=[25,45], project_name="my_project", thre
         gRNA = Pool(6).map(partial(search_b,fix_seq,fix_seq_len,number),[record for record in SeqIO.parse(handle, "fastq")])
         for key in gRNA:
             gRNAs_dict[key] = gRNAs_dict.get(key, 0) + 1
+        del gRNAs_dict[None]
 
     '''
     with open(project_name + ".fq") as handle:
@@ -93,7 +94,7 @@ def search_a(number,line):
 def search_b(fix_seq, fix_seq_len, number, record):
     seq = str(record.seq)
     if seq[0:30] == fix_seq: ##valid record
-        gRNA = str(seq[fix_seq_len+number[0]:fix_seq_len+number[1]])
+        gRNA = seq[fix_seq_len+number[0]:fix_seq_len+number[1]]
         return gRNA
 
 def pcr_count(project_name):
