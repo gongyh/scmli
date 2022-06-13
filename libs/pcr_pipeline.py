2936bd2978747495cd1e140eaf90b5db8f1cb0c7
zhangjiayouc <zhangjs@qibebt.ac.cn>
2022-06-13 10:47:37 +0800

    update multiprocess

--- libs/pcr_pipeline.py
+++ libs/pcr_pipeline.py
@@ -57,13 +57,12 @@ def pcr_parse_gRNA(lib, fix_seq, number=[25,45], project_name="my_project", thre
     fix_seq_len = len(fix_seq)
     gRNAs_dict = {}
     
-    ## extract core function as a function, def core_func(param1,param2): XXX
-    ## define thread pool, p = Pool(threads)
-    ## map and reduce, p.map(core_func, [inputs])
+    with open("../output/my_project.fq") as handle:
+        gRNA = Pool(6).map(partial(search_b,fix_seq,fix_seq_len,number),[record for record in SeqIO.parse(handle, "fastq")])
+        for key in gRNA:
+            gRNAs_dict[key] = gRNAs_dict.get(key, 0) + 1
 
-    fix_seq_len = len(fix_seq)
-    gRNAs_dict = {}
-
+    '''
     with open(project_name + ".fq") as handle:
         for record in SeqIO.parse(handle, "fastq"):
             seq = str(record.seq)
@@ -74,6 +73,7 @@ def pcr_parse_gRNA(lib, fix_seq, number=[25,45], project_name="my_project", thre
                     gRNAs_dict[gRNA] += 1
                 else:
                     gRNAs_dict[gRNA] = 1
+    '''
 
     with open(project_name+".counts", "w") as f:
         for k, v in gRNAs_dict.items():
@@ -90,6 +90,11 @@ def search_a(number,line):
     result = (gRNA, gene)
     return result
 
+def search_b(fix_seq, fix_seq_len, number, record):
+    seq = str(record.seq)
+    if seq[0:30] == fix_seq: ##valid record
+        gRNA = str(seq[fix_seq_len+number[0]:fix_seq_len+number[1]])
+        return gRNA
 
 def pcr_count(project_name):
     '''
