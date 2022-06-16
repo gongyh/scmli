3dea185c677b815adf7825eb29fbf9311506bdaa
zhangjiayouc <zhangjs@qibebt.ac.cn>
2022-06-16 16:58:28 +0800

    remove multi

--- libs/pcr_pipeline.py
+++ libs/pcr_pipeline.py
@@ -4,19 +4,12 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-import datetime
-from Bio import SeqIO
-from Bio.Seq import Seq
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
-from multiprocessing import Pool
-from functools import partial
+
 
 def pcr_qc(project_name, read1, read2, FASTQC_PATH=None, TRIM_GALORE_PATH=None, threads=8):
-    '''
-    read1 = args.read1 (file)
-    read2 = args.read2 (file)
-    '''
-    #fastqc
+
+    # Fastqc
     print("fastqc......")
     if FASTQC_PATH:
         FASTQC_BIN=FASTQC_PATH+"/fastqc"
@@ -24,77 +17,63 @@ def pcr_qc(project_name, read1, read2, FASTQC_PATH=None, TRIM_GALORE_PATH=None, 
         FASTQC_BIN="fastqc"
     os.system(FASTQC_BIN + ' -o . ' + read1 + ' ' + read2 + "> pcr_pipeline.log 2>&1") 
 
-    #trim
+    # Trim_galore
     print("trim_galore......")
     if TRIM_GALORE_PATH:
         TRIM_GALORE_BIN=TRIM_GALORE_PATH+"/trim_galore"
     else:
         TRIM_GALORE_BIN="trim_galore"
     os.system(TRIM_GALORE_BIN + ' --paired --fastqc --max_n 0 -j ' + str(threads) + ' --gzip ' + read1 + ' ' + read2 + ">> pcr_pipeline.log 2>&1")
- 
-   #jieya hebing
+
+    # Unzip and merge files
     name1 = read1.split('/')[-1]
     name1 = name1.split(".fq.gz")[0] + "_val_1.fq.gz"
     name2 = read2.split('/')[-1]
     name2 = name2.split(".fq.gz")[0] + "_val_2.fq.gz"
     os.system('gzip -cd ' + name1 + ' ' + name2 + ' > ' + project_name + '.fq')
 
+
 def pcr_parse_gRNA(lib, fix_seq, number=[25,45], project_name="my_project", threads=8):
     
     '''
+    parse and count target sequence
     lib = args.lib (file.csv)
     fix_seq = args.seq ("GGTAGAATTGGTCGTTGCCATCGACCAGGC")
-    
     '''
 
     print("search......")
-    
-    pool = Pool(threads)
-    gRNA_gene = {}
-    
+
+    # Get the gRNA_gene {seq: id, ...} from lib
+    # {'GAGTGTGGTGGAATTTGCCG': 'NO01G00240', ...}
+
+    gRNA_gene = {}  
     with open(lib) as fh:
-        lines = fh.readlines()
-        result = pool.map(partial(search_a,number),lines)
-        gRNA_gene.update(result)
+        for line in fh:
+            cl = line.split(",")
+            gene = cl[0]
+            gRNA = cl[1][number[0]:number[1]]
+            gRNA_gene[gRNA] = gene
 
     fix_seq_len = len(fix_seq)
     gRNAs_dict = {}
     
-    with open(project_name + ".fq") as handle:
-        a1 = datetime.datetime.now()
-        # records = list(SeqIO.parse(handle, "fastq"))
-        records = list(seq for (title, seq, quality) in FastqGeneralIterator(handle))
-        a2 = datetime.datetime.now()
-        gRNA = pool.map(partial(search_b,fix_seq,fix_seq_len,number), records)
-        a3 = datetime.datetime.now()
-        for key in gRNA:
-            gRNAs_dict[key] = gRNAs_dict.get(key, 0) + 1
-        del gRNAs_dict[None]
-        a4 = datetime.datetime.now()
-        print("1")
-        print(a2-a1)
-        print("2")
-        print(a3-a2)
-        print("3")
-        print(a4-a3)
+    # Get fixed sequence from file.fastq and count
+    # {'GAGTGTGGTGGAATTTGCCG': 3, ...}
 
-    '''
     with open(project_name + ".fq") as handle:
-        # for record in SeqIO.parse(handle, "fastq"):
-        a1 = datetime.datetime.now()
         for (title, seq, quality) in FastqGeneralIterator(handle):    
-            # seq = str(record.seq)
             if seq[0:fix_seq_len] == fix_seq: #valid record
                 #change left and right equal
-                gRNA = str(seq[fix_seq_len+number[0]:fix_seq_len+number[1]])
+                gRNA = seq[fix_seq_len+number[0]:fix_seq_len+number[1]]
                 if gRNA in gRNAs_dict.keys():
                     gRNAs_dict[gRNA] += 1
                 else:
                     gRNAs_dict[gRNA] = 1
-        a2 = datetime.datetime.now()
-        print("1")
-        print(a2-a1)
-    '''
+
+
+    # Change count format and add gRNA_gene id
+    # NO01G00240  GAGTGTGGTGGAATTTGCCG  3
+    # unknow      CCCCCCCCCCGAGTGTGGTG  1
 
     with open(project_name+".counts", "w") as f:
         for k, v in gRNAs_dict.items():
@@ -104,22 +83,10 @@ def pcr_parse_gRNA(lib, fix_seq, number=[25,45], project_name="my_project", thre
             f.write("%s\t%s\t%d\n"%(gene,k,v))
 
 
-def search_a(number,line):
-    cl = line.split(",")
-    gene = cl[0]
-    gRNA = cl[1][number[0]:number[1]]
-    result = (gRNA, gene)
-    return result
-
-def search_b(fix_seq, fix_seq_len, number, record):
-    # seq = str(record.seq)
-    if record[0:30] == fix_seq: ##valid record
-        gRNA = record[fix_seq_len+number[0]:fix_seq_len+number[1]]
-        return gRNA
-
 def pcr_count(project_name):
-    '''
-    '''
+
+    # Statistics
+
     #fixed_seq, e.g. GGTAGAATTGGTCGTTGCCATCGACCAGGC
     print("stats......")
     df1 = pd.read_csv(project_name + ".counts", sep = "\t", header = None, names = ["gene_id", "sequence", "counts", "percent"])
