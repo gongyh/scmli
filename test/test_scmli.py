c6e5bfe133b38e7e85efe035241ec8888ef9e2a0
zhangjiayouc <zhangjs@qibebt.ac.cn>
2022-10-25 14:23:17 +0800

    test

--- test/test_scmli.py
+++ test/test_scmli.py
@@ -13,10 +13,10 @@ def test_grna():
     
     args = parser.parse_args([
         'gRNA',
-        '-l','NoIMET1_gRNAs.csv',
-        '-s','GGTAGAATTGGTCGTTGCCATCGACCAGGC',
-        '-r1','test_R1.fq.gz',
-        '-r2','test_R2.fq.gz'])
+        '-l', 'NoIMET1_gRNAs.csv',
+        '-s', 'GGTAGAATTGGTCGTTGCCATCGACCAGGC',
+        '-r1', 'test_R1.fq.gz',
+        '-r2', 'test_R2.fq.gz'])
    
     stats = args.func(args)
     assert os.path.isfile("output/reads.pdf") == True
@@ -27,12 +27,14 @@ def test_variant():
     parser = create_arg_parser()
     args = parser.parse_args([
         'variant',
-        '-r1','test_R1.fq.gz',
-        '-r2','test_R2.fq.gz',
-        '--ref','genes.gbk',
-        '--target','targets.bed',
-        '--dtarget','filter.bed'])
+        '-r1', 'test_R1.fq.gz',
+        '-r2', 'test_R2.fq.gz',
+        '--ref', 'genes.gbk',
+        '--target', 'targets.bed',
+        '--dtarget', 'filter.bed',
+        '-o', 'var',
+        '-n', 't1'])
     args.func(args)
-    assert os.path.getsize('output/my_project_snippy/snps.vcf') > 10
-    assert os.path.getsize('output/my_project_snippy_hq.vcf') > 10
+    assert os.path.getsize('var/t1_snippy/snps.vcf') > 10
+    assert os.path.getsize('var/t1_snippy_hq.vcf') > 10
 
